d3b8252aa0d4e9ec0155df7135d07d393ff7f1d2
a <a>
2020-03-17 17:25:55 +0200

    Exercise1Done

--- src/main/java/isp/lab4/exercise1/Exercise1.java
+++ src/main/java/isp/lab4/exercise1/Exercise1.java
@@ -3,6 +3,8 @@ package isp.lab4.exercise1;
 public class Exercise1 {
     public static void main(String []args) {
         TemperatureSensor Sensor1 = new TemperatureSensor(20,"Motor");
-        Sensor1.toString();
+        System.out.println(Sensor1.getLocation());
+        System.out.println(Sensor1.getValue());
+        System.out.println(Sensor1.toString());
     }
 }
